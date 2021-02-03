2fc803d7fb2731a592cfb13dcb9a21f8e0cde6d6
cuteolaf <oliverlim818@gmail.com>
2021-02-03 00:55:31 -0300

    WIP: Login form and fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
 
 function App() {
+  const [token, setToken] = useState();
+
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>API Key Dashboard</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/config.js
@@ -0,0 +1,5 @@
+const ENVIRONMENT = process.env.ENVIRONMENT || 'development';
+
+module.exports = {
+  NODE_API_URL: process.env.NODE_API_URL || 'http://localhost:8080'
+};
