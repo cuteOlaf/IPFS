e48a116d9c8d5e2fabf302776dc0213b1a25d053
cuteolaf <oliverlim818@gmail.com>
2021-02-04 00:40:07 -0300

    remove unused code and lint more

--- client/src/components/Table/Table.js
+++ client/src/components/Table/Table.js
@@ -12,42 +12,32 @@ export default class Table extends Component{
       data: [],
       show: false
     };
-      this.showModal = this.showModal.bind(this);
-      this.hideModal = this.hideModal.bind(this);
-    }
+  }
 
-    componentDidMount() {
-        getAPIKeys().then(({ data }) => {
-          const keysData = formatKeysData(data);
-          this.setState({ data: keysData, isLoaded: true });
-        }).catch(error => this.setState({ isLoaded: true, error }));  
-    }
+  componentDidMount() {
+    getAPIKeys().then(({ data }) => {
+      const keysData = formatKeysData(data);
+      this.setState({ data: keysData, isLoaded: true });
+    }).catch(error => this.setState({ isLoaded: true, error }));  
+  }
+  
+  disableKey = ev => {
+    const keyID = ev.currentTarget.value;
+    disableAPIKey(keyID).then(() => {
+      let updatedKeys = [...this.state.data];
+      updatedKeys = updatedKeys.filter(key => key.id !== keyID);
+      this.setState({ data: updatedKeys });
+    });
+  }
 
-    showModal = () => {
-      this.setState({ show: true });
-    };
-
-    hideModal = () => {
-      this.setState({ show: false });
-    };
-
-    disableKey = ev => {
-      const keyID = ev.currentTarget.value;
-      disableAPIKey(keyID).then(() => {
-        let updatedKeys = [...this.state.data];
-        updatedKeys = updatedKeys.filter(key => key.id !== keyID);
-        this.setState({ data: updatedKeys });
-      });
-    }
-
-    createKey = () => {
-      createAPIKey().then(response => {
-        const [type, id] = response.data.split(' ');
-        const newKey = { id, logs: []};
-        const updatedKeys = [newKey, ...this.state.data];
-        this.setState({ data: updatedKeys });
-      });
-    };
+  createKey = () => {
+    createAPIKey().then(response => {
+      const [type, id] = response.data.split(' ');
+      const newKey = { id, logs: []};
+      const updatedKeys = [newKey, ...this.state.data];
+      this.setState({ data: updatedKeys });
+    });
+  };
 
   render() {
     const { error, isLoaded, data } = this.state;
